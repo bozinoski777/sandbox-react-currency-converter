072ea856037c60f607586efd9d3b0c61a30ddee0
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-12-03 12:44:37 +0100

    Creating Currency row element

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow />
+      <div>=</div>
+      <CurrencyRow />
+    </>
   );
 }
 
---
+++ src/CurrencyRow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CurrencyRow() {
+  return (
+    <div>
+    Currency Row
+    </div>
+  )
+}
