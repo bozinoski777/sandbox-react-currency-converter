c4d2e58dd44f045049c5a1cc72486d59bae8451d
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-12-03 12:50:56 +0100

    Styling added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <>
       <h1>Convert</h1>
       <CurrencyRow />
-      <div>=</div>
+      <div className="equals">=</div>
       <CurrencyRow />
     </>
   );
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CurrencyRow() {
   return (
     <div>
-    <input type="number" />
+    <input type="number" className="input" />
       <select>
         <option value="Hi">Hi</option>
       </select>
