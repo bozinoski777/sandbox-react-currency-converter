fe1d61d79c7f414145c51c1580707f004d6785fb
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-12-03 14:15:46 +0100

    Amount manipulation added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ const BASE_URL = "https://api.exchangeratesapi.io/latest"
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [amount, setAmount] = useState(1)
 
 useEffect(() => {
   fetch(BASE_URL)
     .then(res => res.json())
     .then(data => {
+      const firstCurrency = Object.keys(data.rates)[0]
       setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      setFromCurrency(data.base)
+      setToCurrency(firstCurrency)
     })
 }, [])
 return (
@@ -19,10 +25,14 @@ return (
     <h1>Convert</h1>
     <CurrencyRow
       currencyOptions={currencyOptions}
+      selectCurrency={fromCurrency}
+     onChangeCurrency={e => setFromCurrency(e.target.value)}
     />
     <div className="equals">=</div>
     <CurrencyRow
      currencyOptions={currencyOptions}
+     selectedCurrency={toCurrency}
+     onChangeCurrency={e => setToCurrency(e.target.value)}
     />
   </>
   );
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function CurrencyRow(props) {
   const {
-    currencyOptions
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency
   } = props
   return (
     <div>
     <input type="number" className="input" />
-      <select>
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
       {currencyOptions.map(option => (
         <option key={option} value={option}>{option}</option>
 
